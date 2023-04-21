28497455fdc81eeeaa920636211fce256acc170a
Nicholas Truter <ntruter42@gmail.com>
2023-04-21 22:18:43 +0200

    Update settings-bill.js, settings-bill-test.js - added setSmsCost and getSmsCost factory functions, tested setSmsCost and getSmsCost

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -1,6 +1,7 @@
 function BillWithSettings() {
 	var theCallCost = 0;
-	
+	var theSmsCost = 0;
+
 	function setCallCost(callCost) {
 		theCallCost = callCost;
 	}
@@ -9,8 +10,18 @@ function BillWithSettings() {
 		return theCallCost;
 	}
 
+	function setSmsCost(smsCost) {
+		theSmsCost = smsCost;
+	}
+
+	function getSmsCost() {
+		return theSmsCost;
+	}
+
 	return {
 		setCallCost,
-		getCallCost
+		getCallCost,
+		setSmsCost,
+		getSmsCost
 	}
 }
--- js/test/settings-bill-test.js
+++ js/test/settings-bill-test.js
@@ -10,4 +10,36 @@ describe("set values", function () {
 		assert.equal(2.75, settingsBill2.getCallCost());
 
 	});
+
+	it("should be able to set the sms cost", function () {
+
+		let settingsBill = BillWithSettings();
+		settingsBill.setSmsCost(0.85);
+		assert.equal(0.85, settingsBill.getSmsCost());
+
+		let settingsBill2 = BillWithSettings();
+		settingsBill2.setSmsCost(0.75);
+		assert.equal(0.75, settingsBill2.getSmsCost());
+
+	});
+
+	it("should be able to set the call and sms costs", function () {
+
+		let settingsBill = BillWithSettings();
+
+		settingsBill.setCallCost(2.85);
+		settingsBill.setSmsCost(0.85);
+
+		assert.equal(2.85, settingsBill.getCallCost());
+		assert.equal(0.85, settingsBill.getSmsCost());
+
+		let settingsBill2 = BillWithSettings();
+
+		settingsBill2.setCallCost(3.75);
+		settingsBill2.setSmsCost(1.75);
+
+		assert.equal(3.75, settingsBill2.getCallCost());
+		assert.equal(1.75, settingsBill2.getSmsCost());
+
+	});
 });
